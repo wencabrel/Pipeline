8b477e274ff318c72da073909e30f3167d45be4d
WenCabrel <wouladjecabrelwen@gmail.com>
2023-03-06 15:25:37 +0800

    added the counter for the number of file

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 from datetime import datetime
 
+folder = ""
+J_folder = ""
+count = 0
+
 for i in range(0, 11, 1):
     # get the current date and time
     x = datetime.now()
     # let's create our path where we want it to be located
     parent_dir = "E:/opencvproject/workbook/"
-
+    folder = parent_dir
     # let's create our directory/folder
     directory = x.strftime(f'{"%d-%m-%Y-"}{i}{".txt"}')  # "workbook"
     # path
     path = os.path.join(parent_dir, directory)
+    J_folder = path
+
     try:
         # let's create it
         # os.makedirs(path)
@@ -22,3 +28,9 @@ for i in range(0, 11, 1):
         pass
 
 print("all files have been created", i)
+
+for j in os.listdir(folder):
+    if os.path.isfile(J_folder):
+        count += 1
+print('number of file', count)
+
